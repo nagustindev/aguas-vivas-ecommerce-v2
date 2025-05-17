5068fa3a7968aa17d7ab5e21ebe61326b6d5efab
Agus <agusnieva0cinco@gmail.com>
2025-05-17 18:43:22 -0300

    fixed an error in cart render

--- src/layouts/Home.jsx
+++ src/layouts/Home.jsx
@@ -1,19 +1,16 @@
 import Header from '../components/Header.jsx';
-import Cart from '../components/Cart.jsx';
 import Main from '../components/Main.jsx';
 import Footer from '../components/Footer.jsx';
 import Products from '../components/Products.jsx';
-import { CartProvider } from '../context/cart.jsx';
 
 function Home() {
     return (
-      <CartProvider>
-            <Header />
-            <Cart />
-            <Main />
-            <Products />
-            <Footer />
-      </CartProvider>
+    <div>
+      <Header />
+      <Main />
+      <Products />
+      <Footer />
+    </div>
     );
   }
 
